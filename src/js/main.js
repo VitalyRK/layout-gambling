a003ed12c6d986eede603e976199bc9089fa1d49
VitalyRK <shishovip@yandex.ru>
2023-10-07 13:22:08 +0300

    feat: adaptive layout (Sat, Oct  7, 2023  1:22:08 PM)

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
 import '../scss/style.scss'
+
+// CHANGE LANGUAGE
+const changeLanguageText = document.getElementById('change-language-text');
+document.getElementById('change-language-button').addEventListener('click', () => {
+  changeLanguageText.innerHTML = changeLanguageText.innerHTML === 'EN' ? 'RU' : 'EN';
+})
+
+// ADAPTIVE MENU
+
+const openMenuButton = document.getElementById('header__button-menu');
+const iconOpenButton = document.getElementById('header__button-open');
+const iconCloseButton = document.getElementById('header__button-close');
+const navigationMenu = document.getElementById('navigation');
+
+openMenuButton.addEventListener('click', () => {
+  iconOpenButton.classList.toggle('hidden-button');
+  iconCloseButton.classList.toggle('hidden-button');
+  navigationMenu.classList.toggle('navigation__active');
+})
